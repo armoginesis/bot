1e029ff7012e127621ac3d6f8f922dc943992b96
armoginesis <leonidgpt110895kek@gmail.com>
2025-02-03 14:17:39 +0700

    add \t

--- main.py
+++ main.py
@@ -93,8 +93,8 @@ async def set_norm(update: Update, context):
         await update.message.reply_text(f'Норма установлена: {int(norm)}. Вводи расстояние и воза через пробел'
                                         f'\nПример: 2.2 10'
                                         f'\n\nУ тебя больше чем одно расстояние? Добавь с нового абзаца.'
-                                        f'\n Н-р: 5.5 10'
-                                        f'\n      3.2 15',
+                                        f'\nН-р:\t5.5 10'
+                                        f'\n\t3.2 15',
                                         reply_markup=start_keyboard(), )
         return WAITING_FOR_DISTANCE_AND_VOZ
     except ValueError:
